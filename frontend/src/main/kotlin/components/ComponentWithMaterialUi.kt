25166e417a84b5d8d6954ed1c1962a66d305d4bb
Christian Dräger <christian.draeger1@gmail.com>
2020-09-21 18:56:46 +0200

    fix: typo

--- frontend/src/main/kotlin/components/ComponentWithMaterialUi.kt
+++ frontend/src/main/kotlin/components/ComponentWithMaterialUi.kt
@@ -3,13 +3,12 @@ package components
 import com.ccfraser.muirwik.components.MColor
 import com.ccfraser.muirwik.components.button.MButtonVariant
 import com.ccfraser.muirwik.components.button.mButton
-import com.ccfraser.muirwik.components.mBadge
 import react.*
 import react.dom.div
 
 private val component = functionalComponent<RProps> {
     div {
-        mButton("I'm a meterial-UI component", variant = MButtonVariant.contained, color = MColor.primary)
+        mButton("I'm a material-UI component", variant = MButtonVariant.contained, color = MColor.primary)
     }
 }
 
