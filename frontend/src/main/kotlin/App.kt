9fb4a9b18280fc0daff80cb9b547f347172f5c3d
Christian Dräger <christian.draeger1@gmail.com>
2020-09-21 08:02:16 +0200

    feat: minimal change to react apps markup

--- frontend/src/main/kotlin/App.kt
+++ frontend/src/main/kotlin/App.kt
@@ -4,12 +4,14 @@ import react.RComponent
 import react.RProps
 import react.RState
 import react.dom.div
-import react.dom.span
+import react.dom.h2
+import react.dom.h3
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         div {
-            span { +"a little kotlin JS demo with react, styled, components, material-ui - nothing fancy looking, just to showcase functionality" }
+            h2 { +"a little kotlin JS demo with react, styled, components, material-ui" }
+            h3 { +"nothing fancy looking, just to showcase functionality" }
             exampleFunctionalComponentWithProps { name = "KotlinJS" }
             exampleFunctionalComponentWithStateHook
             exampleFunctionalComponentWithStyle
