e4a842103705149267f1f725521055e352e516db
Christian Dräger <christian.draeger1@gmail.com>
2020-09-24 14:58:00 +0200

    feat: backend websocket handler with server-side state handling

--- backend/src/main/kotlin/application/api/ApiController.kt
+++ backend/src/main/kotlin/application/api/ApiController.kt
@@ -1,15 +1,22 @@
 package application.api
 
 import Greeter
+import MySocketMessage
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.concurrent.thread
 
 private val logger = KotlinLogging.logger {}
 
@@ -27,10 +34,50 @@ class RestApiController {
 @Component
 class SocketHandler : TextWebSocketHandler() {
 
+    public fun send(message: MySocketMessage) {
+        val deserialized = message.toJson()
+        sessionState.values.forEach {
+            it.session.sendMessage(TextMessage(deserialized))
+        }
+    }
+
     public override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
-        logger.debug { "session: $session" }
-        logger.debug { "message: $message" }
         val payload = MySocketMessage.parse(message.payload)
-        session.sendMessage(TextMessage("Hi $payload how may we help you?"))
+        if (payload is MySocketMessage.StartTimeSending) {
+            sessionState[session.id]?.time = true
+            sendOnDataTimeChange(session)
+        }
+        if (payload is MySocketMessage.StopTimeSending) {
+            sessionState[session.id]?.time = false
+        }
+    }
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        sessionState[session.id] = State(session.id, session, false)
+    }
+
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        logger.info { "websocket closed" }
+    }
+
+    var currentDateAndTimeState = ""
+    fun sendOnDataTimeChange(session: WebSocketSession) {
+        thread {
+            while (sessionState[session.id]?.time == true) {
+                val time = LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+                if (currentDateAndTimeState != time) {
+                    currentDateAndTimeState = time
+                    session.sendMessage(TextMessage(MySocketMessage.ServerTime("foo", "bar", "ndsijfbhs").toJson()))
+                }
+            }
+        }
     }
 }
+
+var sessionState = ConcurrentHashMap<String, State>()
+
+data class State(
+    val id: String,
+    val session: WebSocketSession,
+    var time: Boolean = false
+)
